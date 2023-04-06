e04aec18009707561ab7f98baf32eb93c88046d1
Hardik <Hardikproject@.com>
2023-04-06 17:40:39 +0530

    conwort div to section and hover efact

---
+++ public/assets/js/app.js
@@ -0,0 +1,114 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+$('body').on('click', '.hamburgur', function () {
+  $(".sidebar").addClass("active");
+  $(".serchbar").removeClass("active");
+});
+$('body').on('click', '.close', function () {
+  $(".sidebar").removeClass("active");
+});
+$('body').on('click', '.serch-icon', function () {
+  $(".serchbar").toggleClass("active");
+  $(".sidebar").removeClass("active");
+});
+
+/* CHART---------------------*/
+
+var options = {
+  series: [{
+    name: 'series1',
+    data: [11, 32, 45, 32, 34, 52, 41]
+  }, {
+    name: 'series2',
+    data: [31, 40, 28, 51, 42, 109, 100]
+  }],
+  chart: {
+    height: 270,
+    type: 'area'
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  xaxis: {
+    type: 'datetime',
+    categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+  },
+  tooltip: {
+    x: {
+      format: 'dd/MM/yy HH:mm'
+    }
+  }
+};
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+/* CHART2 ---------------------*/
+
+var options = {
+  series: [{
+    name: 'Net Profit',
+    data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+  }, {
+    name: 'Revenue',
+    data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+  }, {}],
+  chart: {
+    type: 'bar',
+    height: 222
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: '25%',
+      endingShape: 'rounded'
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ['transparent']
+  },
+  fill: {
+    opacity: 1
+  },
+  tooltip: {
+    y: {
+      formatter: function formatter(val) {
+        return "$ " + val + " thousands";
+      }
+    }
+  }
+};
+var chart = new ApexCharts(document.querySelector("#chart2"), options);
+chart.render();
+
+/* CHART3 -------------*/
+
+var options = {
+  series: [60],
+  chart: {
+    height: 206,
+    type: 'radialBar'
+  },
+  plotOptions: {
+    radialBar: {
+      hollow: {
+        size: '70%'
+      }
+    }
+  },
+  labels: ['Total Score']
+};
+var chart = new ApexCharts(document.querySelector("#chart3"), options);
+chart.render();
+/******/ })()
+;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge:[],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors:{
+      Primary:'#0075FF',
+      white:'#fff',
+      Secondary:'#A0AEC0',
+      red:'#E31A1A',
+      green:'#01B574',
+      iconbg:'#1A1F37',
+      lightblack:'#0F1535',
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
